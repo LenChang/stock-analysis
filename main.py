388db801931f02aaee0cfbfd3655e17ff89fafbb
Len Chang <lamgo.chang@gmail.com>
2025-07-20 12:50:30 +0800

    refactor
    - replace the fixed name by variable

--- main.py
+++ main.py
@@ -48,14 +48,14 @@ plt.plot(x, norm.pdf(x, mean, std), "r-", lw=2, label="Normal Distribution")
 
 # Calculate percentage of data within one standard deviation
 within_1std = close_prices[
-    (close_prices["MU"] >= (mean["MU"] - std["MU"]))
-    & (close_prices["MU"] <= (mean["MU"] + std["MU"]))
+    (close_prices[ticker_symbol] >= (mean[ticker_symbol] - std[ticker_symbol]))
+    & (close_prices[ticker_symbol] <= (mean[ticker_symbol] + std[ticker_symbol]))
 ]
 percent_1std = len(within_1std) / len(close_prices) * 100
 
 within_2std = close_prices[
-    (close_prices["MU"] >= (mean["MU"] - 2 * std["MU"]))
-    & (close_prices["MU"] <= (mean["MU"] + 2 * std["MU"]))
+    (close_prices[ticker_symbol] >= (mean[ticker_symbol] - 2 * std[ticker_symbol]))
+    & (close_prices[ticker_symbol] <= (mean[ticker_symbol] + 2 * std[ticker_symbol]))
 ]
 percent_2std = len(within_2std) / len(close_prices) * 100
 
@@ -63,7 +63,7 @@ percent_2std = len(within_2std) / len(close_prices) * 100
 print(f"{within_1std}")
 
 print(
-    f"Mean: {mean['MU']}, Standard Deviation: {std['MU']}, within_1std: {len(within_1std)}, close_prices: {len(close_prices)}"
+    f"Mean: {mean[ticker_symbol]}, Standard Deviation: {std[ticker_symbol]}, within_1std: {len(within_1std)}, close_prices: {len(close_prices)}"
 )
 print(
     f"Percentage of data within one and two standard deviation: {percent_1std}% {percent_2std}%"
